8634b43d84c302589d32b7b77666504b86b758da
jverbosky <jverbosk@gmail.com>
2017-02-15 16:32:06 -0500

    added line breaks in final output

--- app.rb
+++ app.rb
@@ -13,7 +13,6 @@ end
 post '/post_age' do
   backend_age = params[:user_age]
   backend_name = params[:user_name]
-  # "Your name is #{backend_name} and your location is #{backend_age}."
   erb :get_numbers, :locals => {:name => backend_name, :age => backend_age}
 end
 
@@ -25,9 +24,7 @@ post '/post_numbers' do
   three = params[:num_3]
   sum = one.to_i + two.to_i + three.to_i
   compare = (sum < backend_age.to_i) ? "less" : "greater"
-  "#{backend_name}, your age is #{backend_age}. Your favorite numbers are #{one}, #{two} and #{three}. The sum of your favorite numbers is #{sum}, which is #{compare} than your age."
+  "#{backend_name}, your age is #{backend_age}.<br>
+   Your favorite numbers are #{one}, #{two} and #{three}.<br>
+   The sum of your favorite numbers is #{sum}, which is #{compare} than your age."
 end
-
-# User* your age is age*.
-# Your three favorite numbers are num1*, num2* and num3*.
-# The sum of your favorite numbers is sum*, which is less/greater than* your age.
