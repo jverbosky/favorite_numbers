45b807cd2237f8e144c5f9dd4de250c548c45540
jverbosk <jverbosk@gmail.com>
2017-02-16 17:15:02 -0500

    redoing favorite numbers using redirects - in progress, using sandbox test files

---
+++ app_redirects.rb
@@ -0,0 +1,40 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?  # automatically reload app.rb on save via sinatra-contrib gem
+
+get "/" do
+  erb :get_name
+end
+
+post '/post_name' do
+  backend_name = params[:user_name]
+  redirect "/age?name=" + backend_name
+end
+
+get '/age' do
+  backend_name = params[:name]
+  erb :get_age, :locals => {:name => backend_name}  # name is passed to /get_location
+end
+
+post '/post_age' do
+  backend_name = params[:user_name]
+  backend_age = params[:user_age]
+  redirect "/numbers?age=" + backend_age
+end
+
+get 'numbers' do
+  backend_age = params[:user_age]
+  erb :get_numbers, :locals => {:name => backend_name, :age => backend_age}
+end
+
+post '/post_numbers' do
+  backend_name = params[:user_name]
+  backend_age = params[:user_age]
+  one = params[:num_1]
+  two = params[:num_2]
+  three = params[:num_3]
+  sum = one.to_i + two.to_i + three.to_i
+  compare = (sum < backend_age.to_i) ? "less" : "greater"
+  "#{backend_name}, your age is #{backend_age}.<br>
+   Your favorite numbers are #{one}, #{two} and #{three}.<br>
+   The sum of your favorite numbers is #{sum}, which is #{compare} than your age."
+end
