38681223a1511f45af51ba409309d57826fed1d2
jverbosk <jverbosk@gmail.com>
2017-02-16 19:44:21 -0500

    making progress on app_redirects using hash to pass multiple values

--- app_redirects.rb
+++ app_redirects.rb
@@ -6,37 +6,39 @@ get "/" do
 end
 
 post '/post_name' do
-  backend_name = params[:user]  # user = [{"user_name"=>"John"}]
-  #"#{params[:user]}"  # [{"user_name"=>"John"}]
-  "#{backend_name[0]["user_name"]}" # John
-  # redirect "/age?name=" + backend_name
-  # "Your name is #{user["user_name"]}."
+  backend_name = params[:user_name]
+  redirect "/age?name=" + backend_name
 end
 
-# get '/age' do
-#   backend_name = params[:name]
-#   erb :get_age, :locals => {:name => backend_name}  # name is passed to /get_location
-# end
+get '/age' do
+  backend_name = params[:name]  # John
+  erb :get_age_redirects, :locals => {:name => backend_name}  # name is passed to /get_location
+end
 
-# post '/post_age' do
-#   backend_name = params[:user_name]
-#   redirect "/numbers?age=" + backend_age
-# end
+post '/post_age' do
+  user_hash = params[:user]
+  redirect "/numbers?user=" + user_hash.to_s  # convert hash to string for use in URL
+  # "#{user_hash}"  # {"user_age"=>"41", "user_name"=>"John"}
+  # "#{user_hash["user_name"]}, your age is #{user_hash["user_age"]}."  # John, your age is 41.
+end
 
-# get 'numbers' do
-#   backend_age = params[:user_age]
-#   erb :get_numbers, :locals => {:name => backend_name, :age => backend_age}
-# end
+get '/numbers' do
+  user_hash = eval(params[:user])  # use eval to convert string to hash
+  backend_name = user_hash["user_name"]
+  backend_age = user_hash["user_age"]
+  "#{backend_name}, #{backend_age}"
+  # erb :get_numbers, :locals => {:name => backend_name, :age => backend_age}
+end
 
-# post '/post_numbers' do
-#   backend_name = params[:user_name]
-#   backend_age = params[:user_age]
-#   one = params[:num_1]
-#   two = params[:num_2]
-#   three = params[:num_3]
-#   sum = one.to_i + two.to_i + three.to_i
-#   compare = (sum < backend_age.to_i) ? "less" : "greater"
-#   "#{backend_name}, your age is #{backend_age}.<br>
-#    Your favorite numbers are #{one}, #{two} and #{three}.<br>
-#    The sum of your favorite numbers is #{sum}, which is #{compare} than your age."
-# end
+post '/post_numbers' do
+  backend_name = params[:user_name]
+  backend_age = params[:user_age]
+  one = params[:num_1]
+  two = params[:num_2]
+  three = params[:num_3]
+  sum = one.to_i + two.to_i + three.to_i
+  compare = (sum < backend_age.to_i) ? "less" : "greater"
+  "#{backend_name}, your age is #{backend_age}.<br>
+   Your favorite numbers are #{one}, #{two} and #{three}.<br>
+   The sum of your favorite numbers is #{sum}, which is #{compare} than your age."
+end
